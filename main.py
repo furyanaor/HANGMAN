1140f28be30cbcc634816fca88313bbb9363489c
FuryaNaor <furyanaor@gmail.com>
2020-10-01 14:46:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,11 @@ HANGMAN_ASCII_ART ="""\\   _    _
 MAX_TRIES = 6
 print(HANGMAN_ASCII_ART)
 
+print("set a new word:")
+word = input()
+print("thw word is:", word)
+print('_ ' * len(word))
+
 print("Let's geuss some letter: ")
 letter = input()
-print("your letter is: ", letter)
-# num = int(input())
-# print("num is:", num)
-# blocks = 0
-# for i in range(3):
-#     blocks += num % 10
-#     num //= 10
-# print(blocks)
-# block_per_pig = int(blocks / 3)
-# print(block_per_pig)
-# print(blocks % 3)
-# print(not bool(blocks % 3))
+print("your letter is: ", letter.lower())
