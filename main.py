d9d7f86360825179e664613939bb10ad2fae2800
FuryaNaor <furyanaor@gmail.com>
2020-10-03 00:18:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ MAX_TRIES = 6
 print(HANGMAN_ASCII_ART)
 
 word = input("set a new word: ")
-print("thק word is:", word)
+print("the word is:", word)
 print('_ ' * len(word))
 
 letter = input("Let's geuss some letter: ")
-print("your letter is:", letter.lower())
+if len(letter) > 1:
+    if not letter.isalpha():
+        print("E3")
+    else:
+        print("E1")
+elif not letter.isalpha():
+    print("E2")    
+else:
+    print("your letter is:", letter.lower())
