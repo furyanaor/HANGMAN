1f3e99459f3b1f5a6c8b169a4d1c3ce85a593f07
FuryaNaor <furyanaor@gmail.com>
2020-10-03 19:51:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-HANGMAN_ASCII_ART = """\\   _    _
+HANGMAN_ASCII_ART = """ \\
+    _    _
    | |  | |
    | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
-   |  __  |/ _' | '_ \ / _' | '_ ' _ \ / _' | '_ \\
+   |  __  |/ _' | '_ \ / _' | '_ ' _ \ / _' | '_  \\
    | |  | | (_| | | | | (_| | | | | | | (_| | | | |
    |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                         __/ |
@@ -11,17 +12,23 @@ HANGMAN_ASCII_ART = """\\   _    _
 MAX_TRIES = 6
 print(HANGMAN_ASCII_ART)
 
+def is_valid_input(letter_guessed):
+    if len(letter_guessed) > 1:
+        if not letter_guessed.isalpha():
+            print("E3")
+        else:
+            print("E1")
+        return False
+    elif not letter_guessed.isalpha():
+        print("E2")
+        return False
+    else:
+        print("your letter is:", letter_guessed.lower())
+        return True
+
 word = input("set a new word: ")
 print("the word is:", word)
 print('_ ' * len(word))
 
 letter = input("Let's geuss some letter: ")
-if len(letter) > 1:
-    if not letter.isalpha():
-        print("E3")
-    else:
-        print("E1")
-elif not letter.isalpha():
-    print("E2")    
-else:
-    print("your letter is:", letter.lower())
+print(is_valid_input(letter))
